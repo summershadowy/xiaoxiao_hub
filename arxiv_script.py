5704746ad4fc82f3ff87820a8f0411c668b1441b
summershadowy <153717386+summershadowy@users.noreply.github.com>
2023-12-20 17:08:28 +0800

    arxiv_script.py

--- arxiv_script.py
+++ arxiv_script.py
@@ -1,36 +1,72 @@
+import requests
+import xml.etree.ElementTree as ET
+import csv
+from datetime import datetime, timedelta
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-def send_email(file_path, receiver_email):
-    sender_email = "your_email@example.com"  # Replace with your email
-    sender_password = "your_password"        # Replace with your email password
+def fetch_arxiv_papers(yesterday_date):
+    api_endpoint = 'http://export.arxiv.org/api/query?'
+    query = f'search_query=all:LLM+AND+all:medical&start=0&max_results=50&sortBy=submittedDate&sortOrder=descending'
+    response = requests.get(api_endpoint + query)
 
-    # Create a multipart message
+    if response.status_code == 200:
+        root = ET.fromstring(response.text)
+        papers = []
+        for entry in root.findall('{http://www.w3.org/2005/Atom}entry'):
+            title = entry.find('{http://www.w3.org/2005/Atom}title').text.strip()
+            abstract = entry.find('{http://www.w3.org/2005/Atom}summary').text.strip() 
+            published = entry.find('{http://www.w3.org/2005/Atom}published').text.strip()
+            link = entry.find('{http://www.w3.org/2005/Atom}id').text.strip()
+            published_date = datetime.strptime(published, '%Y-%m-%dT%H:%M:%SZ').date()
+            if published_date.strftime('%Y-%m-%d') == yesterday_date:
+                papers.append([title, abstract, published_date.strftime('%Y-%m-%d'), link])
+        return papers
+    else:
+        raise Exception("Failed to retrieve data from arXiv API.")
+
+def send_email_with_attachment(gmail_user, gmail_password, receiver_email, subject, body, attachment_path):
     message = MIMEMultipart()
-    message['From'] = sender_email
+    message['From'] = gmail_user
     message['To'] = receiver_email
-    message['Subject'] = "ArXiv Papers List"
-
-    # Body
-    body = "Please find attached the list of yesterday's papers from ArXiv."
+    message['Subject'] = subject
     message.attach(MIMEText(body, 'plain'))
 
-    # Attachment
-    filename = file_path.split('/')[-1]
-    attachment = open(file_path, "rb")
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload(attachment.read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', f"attachment; filename= {filename}")
-    message.attach(part)
+    with open(attachment_path, "rb") as attachment:
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f"attachment; filename={attachment_path}")
+        message.attach(part)
 
-    # SMTP session
-    server = smtplib.SMTP('smtp.example.com', 587)  # Replace with your SMTP server and port 
+    server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login(sender_email, sender_password)
-    text = message.as_string()
-    server.sendmail(sender_email, receiver_email, text)
+    server.login(gmail_user, gmail_password)
+    server.send_message(message)
     server.quit()
+
+# Main process
+yesterday_date = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+papers = fetch_arxiv_papers(yesterday_date)
+
+# Define CSV file path
+csv_file_path = f'arxiv_papers_{yesterday_date}.csv'
+
+# Save papers to CSV
+with open(csv_file_path, 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Title', 'Abstract', 'Date', 'Link'])
+    writer.writerows(papers)
+
+# Email details
+gmail_user = "xiaoxiao.han1994@gmail.com"
+gmail_password = "tzdc fxhw jxcq odht"
+receiver_email = "xiaoxiao.han@outlook.com"
+subject = f"ArXiv Papers List {yesterday_date}"
+body = f"Please find attached the list of papers from ArXiv for the date {yesterday_date}."
+
+# Send email
+send_email_with_attachment(gmail_user, gmail_password, receiver_email, subject, body, csv_file_path)
